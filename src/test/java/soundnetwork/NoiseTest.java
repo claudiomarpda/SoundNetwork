18995bc4b16adf952ca30ce4cc3c323420d424e3
Claudiomar Araújo <claudiomar.araujo@lavid.ufpb.br>
2018-09-04 11:50:18 -0300

    Noise test for finding out the sound threshold

---
+++ src/test/java/soundnetwork/NoiseTest.java
@@ -0,0 +1,43 @@
+package soundnetwork;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.sound.sampled.*;
+
+import static soundnetwork.AudioFormatConfig.*;
+
+/**
+ * Noise test for finding out the sound threshold of a particular ambient and microphone sensibility.
+ */
+public class NoiseTest {
+
+    // How much time to listen
+    private static final long TIME_IN_MILLIS = 10000;
+
+    private SourceDataLine sourceLine;
+    private TargetDataLine targetLine;
+
+    @Before
+    public void setup() throws LineUnavailableException {
+        AudioFormat format = new AudioFormat(SAMPLE_RATE, SAMPLE_SIZE_IN_BITS, CHANNELS, SIGNED, BIG_ENDIAN);
+        DataLine.Info sourceInfo = new DataLine.Info(SourceDataLine.class, format);
+        sourceLine = (SourceDataLine) AudioSystem.getLine(sourceInfo);
+        sourceLine.open();
+        DataLine.Info targetInfo = new DataLine.Info(TargetDataLine.class, format);
+        targetLine = (TargetDataLine) AudioSystem.getLine(targetInfo);
+        targetLine.open();
+    }
+
+    @Test
+    public void listen() {
+        targetLine.start();
+        byte[] data = new byte[1];
+        long end = System.currentTimeMillis() + TIME_IN_MILLIS;
+
+        while (System.currentTimeMillis() < end) {
+            targetLine.read(data, 0, data.length);
+            System.out.println("Input: " + data[0]);
+        }
+    }
+}
